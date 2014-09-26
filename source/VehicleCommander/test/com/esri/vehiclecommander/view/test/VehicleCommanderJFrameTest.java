4efb1b18b123e6b9fcfc26a3d2eae85cb30b150f
Gary Sheppard <gsheppard@esri.com>
2014-09-26 14:15:22 -0400

    Updated licensing unit test for ArcGIS Runtime 10.2.3

--- source/VehicleCommander/test/com/esri/vehiclecommander/view/test/VehicleCommanderJFrameTest.java
+++ source/VehicleCommander/test/com/esri/vehiclecommander/view/test/VehicleCommanderJFrameTest.java
@@ -1,8 +1,24 @@
+/*******************************************************************************
+ * Copyright 2013-2014 Esri
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ******************************************************************************/
 package com.esri.vehiclecommander.view.test;
 
 import com.esri.runtime.ArcGISRuntime;
 import com.esri.client.local.LocalServer;
-import com.esri.client.local.LocalServerStatus;
+import com.esri.core.runtime.LicenseResult;
+import com.esri.vehiclecommander.view.VehicleCommanderJFrame;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -41,10 +57,11 @@ public class VehicleCommanderJFrameTest {
      */
     @Test
     public void testLicenseInvalid() {
-        ArcGISRuntime.setLicense(VehicleCommanderJFrame.BUILT_IN_LICENSE_STRING);
-        ArcGISRuntime.initialize();
-        if (ArcGISRuntime.isLicensed()) {
-            fail("Warning: your code contains a valid license file!");
+        String fakeClientId = "abcdefghijklmnop";
+        ArcGISRuntime.setClientID(fakeClientId);
+        LicenseResult licenseResult = ArcGISRuntime.License.setLicense(VehicleCommanderJFrame.BUILT_IN_LICENSE_STRING);
+        if (!LicenseResult.INVALID.equals(licenseResult)) {
+            fail("Warning: your code contains a license string that is " + licenseResult + "! It should be " + LicenseResult.INVALID + " instead.");
         }
     }
 
